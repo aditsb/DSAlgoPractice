550896299752970d27250a1a4d5d5712e40816e3
aditsb <asf1000@gmail.com>
2021-09-19 10:21:07 +0530

    DSAlgoPractice LinkedList Commit

---
+++ src/main/java/com/dsalgo/practice/linkedlist/LinkedList.java
@@ -0,0 +1,165 @@
+package com.dsalgo.practice.linkedlist;
+
+public class LinkedList {
+    Node header;
+
+    public Node getHeader() {
+        return header;
+    }
+
+    public void setHeader(Node header) {
+        this.header = header;
+    }
+
+    public LinkedList(Node Node) {
+        this.header = Node;
+    }
+
+    public LinkedList() {
+    }
+
+    public void addToHead(int i) {
+        Node node = new Node(i);
+        if (header == null) {
+            header = node;
+        } else {
+            Node tempNode = header;
+            header = node;
+            header.nextNode = tempNode;
+        }
+    }
+
+    public void addtoTail(int i) {
+        Node node = new Node(i);
+        if (header == null) {
+            header = node;
+        } else {
+            Node tempNode = header;
+            while (tempNode.nextNode != null) {
+                tempNode = tempNode.nextNode;
+            }
+            tempNode.nextNode = node;
+        }
+    }
+
+    public void addNodeAfter(int searchValue, int value) {
+        Node node = new Node(value);
+        if (header != null) {
+            Node tempNode = header;
+            while (tempNode != null) {
+                if (tempNode.data == searchValue) {
+                    Node currNode = tempNode.nextNode;
+                    tempNode.nextNode = node;
+                    node.nextNode = currNode;
+                    break;
+
+                }
+                tempNode = tempNode.nextNode;
+            }
+        }
+    }
+
+    public void deleteHead() {
+        if (header == null) {
+            return;
+        } else {
+            //com.dsalgo.practice.linkedlist.Node tempNode=header;
+            header = header.nextNode;
+
+        }
+    }
+
+    public void deleteTail() {
+        if (header == null) {
+            return;
+        }
+        if (header.nextNode == null) {
+            header = null;
+        } else {
+            Node currentNode = header;
+            Node prevNode = new Node();
+
+            while (currentNode.nextNode != null) {
+                prevNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+            prevNode.nextNode = null;
+
+        }
+    }
+
+    public void deleteNodeAfter(int searchValue) {
+        Node node = new Node(searchValue);
+        if (header != null) {
+            Node tempNode = header;
+            while (tempNode != null) {
+                if (tempNode.data == searchValue) {
+                    Node currNode = tempNode.nextNode != null ? tempNode.nextNode.nextNode : tempNode.nextNode;
+                    tempNode.nextNode = currNode;
+                    break;
+
+                }
+                tempNode = tempNode.nextNode;
+            }
+        }
+    }
+
+    public void detectandRemoveLoop(){
+        Node slow=header;
+        Node fast=header;
+        boolean isLoopExists=false;
+        while(slow!=null && fast.nextNode!=null){
+            slow=slow.nextNode;
+            fast=fast.nextNode.nextNode;
+            if(slow==fast){
+
+                isLoopExists=true;
+                break;
+            }
+            slow=header;
+            while(slow!=fast){
+
+            }
+        }
+    }
+
+
+    public void printLinkedList() {
+        Node tempNode = header;
+        while (tempNode != null) {
+            System.out.println(tempNode.data);
+            tempNode = tempNode.nextNode;
+        }
+    }
+
+    Node reverse(Node head)
+    {
+        if (head == null || head.nextNode == null)
+            return head;
+
+        /* reverse the rest list and put
+        the first element at the end */
+        Node rest = reverse(head.nextNode);
+        head.nextNode.nextNode = head;
+
+        /* tricky step -- see the diagram */
+        head.nextNode = null;
+
+        /* fix the head pointer */
+        return rest;
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addtoTail(10);
+        linkedList.addtoTail((20));
+        linkedList.addtoTail((30));
+        linkedList.addtoTail((40));
+        linkedList.printLinkedList();
+
+        linkedList.header=linkedList.reverse(linkedList.header);
+        linkedList.printLinkedList();
+
+
+    }
+}
---
+++ src/main/java/com/dsalgo/practice/linkedlist/Node.java
@@ -0,0 +1,21 @@
+package com.dsalgo.practice.linkedlist;
+
+public class Node {
+    int data;
+    Node nextNode;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
