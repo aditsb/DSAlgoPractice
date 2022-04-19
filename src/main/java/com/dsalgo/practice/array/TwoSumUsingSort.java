ea8b65df716d7365d4364b6385b4a7aebc060865
Aditya <asf1000@gmail.com>
2022-04-19 19:36:57 +0530

    Tree height programme added

--- src/main/java/com/dsalgo/practice/array/TwoSumUsingSort.java
+++ src/main/java/com/dsalgo/practice/array/TwoSumUsingSort.java
@@ -1,32 +1,28 @@
 package com.dsalgo.practice.array;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TwoSumUsingSort {
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        // Write your code here.
-        Arrays.sort(array);
-        int left=0;
-        int right=array.length-1;
-        while(left<right){
-            int currentSum=array[left]+array[right];
-            if(targetSum==currentSum){
-                return new int[]{array[left],array[right]};
-            }else if(currentSum<targetSum){
-                left++;
-            }else if(currentSum>targetSum){
-                  right--;
-            }
-        }
+       Arrays.sort(array); // Write your code here.
+        int rightIndex=0;
+        int endIndex=array.length-1;
+        for(int count=0;count<array.length;count++){
+            if(array[rightIndex]+(array[endIndex])==targetSum){
+                return new int[]{array[rightIndex],array[endIndex]};
+            }else if(array[rightIndex]+array[endIndex]<targetSum){
+                rightIndex++;
 
+            }else if(array[rightIndex]+array[endIndex]>targetSum){
+                endIndex--;
+            }
+            System.out.println(count);
+        }
         return new int[0];
     }
 
-    public static void main(String[] args) {
-        int []arr=new int[]{3,5,-4,8,11,1,-1,6};
-
-        System.out.println(Arrays.toString(twoNumberSum(arr,10)));
+    public static void main(String []args){
+      int []array=new int[]{3,5,-4,8,11,1,-1,6};
+        System.out.println(Arrays.toString(twoNumberSum(array,110)));
     }
 }
